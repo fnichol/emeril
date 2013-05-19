d1a70d2926ea63c1e779888768fcfc4d9769db05
Fletcher Nichol <fnichol@nichol.ca>
2013-05-18 22:35:55 -0600

    Add spec coverage for Category.

--- emeril.gemspec
+++ emeril.gemspec
@@ -28,6 +28,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'guard-minitest'
   spec.add_development_dependency 'mocha'
   spec.add_development_dependency 'fakefs'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 
   spec.add_development_dependency 'cane'
   spec.add_development_dependency 'guard-cane'
--- spec/emeril/category_spec.rb
+++ spec/emeril/category_spec.rb
@@ -1,12 +1,28 @@
 # -*- encoding: utf-8 -*-
 
 require_relative '../spec_helper'
+require 'vcr'
 
 require 'emeril/category'
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
+
 describe Emeril::Category do
 
-  it "is waiting for specs" do
-    skip
+  describe ".for_cookbook" do
+    it "returns a category string for a known cookbook" do
+      VCR.use_cassette('known_cookbook') do
+        Emeril::Category.for_cookbook('mysql').must_equal 'Databases'
+      end
+    end
+
+    it "returns nil for a nonexistant cookbook" do
+      VCR.use_cassette('nonexistant_cookbook') do
+        Emeril::Category.for_cookbook('fooboobunnyyaya').must_be_nil
+      end
+    end
   end
 end
